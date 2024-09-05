25eb067b9fc7b116f0904d5d6b1b74eb9b9564d0
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-05 01:45:46 -0700

    Create Login Card

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
-
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
