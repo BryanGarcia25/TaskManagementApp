a0ec0428900f18fc8cf87d4e61e8cf86adeb815f
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-06 15:55:09 -0700

    Create header card

--- src/app/features/task-management/task-management.component.ts
+++ src/app/features/task-management/task-management.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-task-management',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatCard, MatCardContent, MatIcon, MatDivider],
   templateUrl: './task-management.component.html',
   styleUrl: './task-management.component.css'
 })
 export class TaskManagementComponent {
-
+  public titleCard: string = "Task Management";
 }
