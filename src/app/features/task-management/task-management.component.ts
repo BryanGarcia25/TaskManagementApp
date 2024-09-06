290bcac7c206791c95bb8a20b0f4a1aa8eb71034
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-06 15:18:31 -0700

    Create task-management component

---
+++ src/app/features/task-management/task-management.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-task-management',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './task-management.component.html',
+  styleUrl: './task-management.component.css'
+})
+export class TaskManagementComponent {
+
+}
