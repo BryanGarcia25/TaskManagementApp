23a92294dd637072384ee3a16b8caa78cb64bbd3
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-06 15:20:43 -0700

    Create route to task-management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TaskManagementComponent } from './features/task-management/task-management.component';
+import { AuthComponent } from './features/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthComponent,
+        title: 'Autenticación'
+    },
+    {
+        path: 'main',
+        component: TaskManagementComponent,
+        title: 'Task Management'
+    }
+];
--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -4,11 +4,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule],
+  imports: [MatCardModule, MatInputModule, MatIconModule, MatButtonModule, MatDividerModule, RouterModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
