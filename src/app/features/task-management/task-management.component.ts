7261d7fa2608aabb96ce4de76ffb70b1c57057a3
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-06 18:49:18 -0700

    Add form to create tasks

--- src/app/features/task-management/task-management.component.ts
+++ src/app/features/task-management/task-management.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-management',
   standalone: true,
-  imports: [RouterModule, MatCard, MatCardContent, MatIcon, MatDivider, MatCheckboxModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [RouterModule, MatCard, MatCardContent, MatIcon, MatDivider, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule],
   templateUrl: './task-management.component.html',
-  styleUrl: './task-management.component.css'
+  styleUrl: './task-management.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskManagementComponent {
   public titleCard: string = "Task Management";
