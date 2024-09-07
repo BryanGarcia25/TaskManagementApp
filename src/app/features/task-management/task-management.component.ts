6ef734bfe9b039af1f3b4d579e18bc47f1bb0edd
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-06 18:15:18 -0700

    Add list tasks card

--- src/app/features/task-management/task-management.component.ts
+++ src/app/features/task-management/task-management.component.ts
@@ -3,14 +3,16 @@ import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-task-management',
   standalone: true,
-  imports: [RouterModule, MatCard, MatCardContent, MatIcon, MatDivider],
+  imports: [RouterModule, MatCard, MatCardContent, MatIcon, MatDivider, MatCheckboxModule],
   templateUrl: './task-management.component.html',
   styleUrl: './task-management.component.css'
 })
 export class TaskManagementComponent {
   public titleCard: string = "Task Management";
+  items = [1,2,3,4,5,6]
 }
