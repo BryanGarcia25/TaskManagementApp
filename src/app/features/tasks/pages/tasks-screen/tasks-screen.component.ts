c1364af1e8e10f3c32b2ebb1d2a75cd858e6636c
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-20 01:05:42 -0700

    Add function to logout button}

--- src/app/features/tasks/pages/tasks-screen/tasks-screen.component.ts
+++ src/app/features/tasks/pages/tasks-screen/tasks-screen.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,6 +10,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-tasks-screen',
@@ -35,4 +36,11 @@ import { MatMenuModule } from '@angular/material/menu';
 export class TasksScreenComponent {
   public titleCard: string = "Task Management";
   items = [1,2,3,4,5]
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/login'])
+  }
 }
