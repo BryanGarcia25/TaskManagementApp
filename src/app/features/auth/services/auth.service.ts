8a808e21e99ed1ef0895a8708c6360b78934c434
Bryan Garcia <osvaldo52programacion@gmail.com>
2024-09-19 17:47:24 -0700

    Create service to user registration function

---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthService {
+    private apiUserUrl = 'http://localhost:3000/auth'
+
+    constructor(private http: HttpClient) { }
+
+    registerUser(userData: any) {
+        return this.http.post(`${this.apiUserUrl}/register`, userData);
+    }
+}
